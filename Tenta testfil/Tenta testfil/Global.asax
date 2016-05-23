c06c2bb632e7ea01945d90cb37b23ceea567569a
johanthorner <johan@thorner.se>
2016-05-23 11:56:33 +0200

    skicka första test

---
+++ Tenta testfil/Tenta testfil/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tenta_testfil.MvcApplication" Language="C#" %>
---
+++ Tenta testfil/Tenta testfil/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tenta_testfil.Startup))]
+namespace Tenta_testfil
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
