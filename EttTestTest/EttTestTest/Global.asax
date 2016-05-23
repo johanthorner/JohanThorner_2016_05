36ab042c3f31cc8c704407a381de4871da880abe
johanthorner <johan@thorner.se>
2016-05-23 12:05:46 +0200

    hejhej test

---
+++ EttTestTest/EttTestTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EttTestTest.MvcApplication" Language="C#" %>
---
+++ Tenta testfil/Tenta testfil/Models/Testklass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tenta_testfil.Models
+{
+    public class Testklass
+    {
+    }
+}
